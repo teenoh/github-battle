75a00ad5f822aa796ca7cd995fbd386fa4485d15
teenoh <valentino@grit.systems>
2017-07-19 03:48:16 +0100

    completed Profile component github data

--- src/components/Results.js
+++ src/components/Results.js
@@ -8,12 +8,16 @@ import PlayerPreview from './PlayerPreview'
 
 const Profile = ({info}) => {
     return (
-        <PlayerPreview 
-                avatar={info.avatar_url}
-                id={info.label}
-                username={info.login}
-                style={{textAlign: "center"}}
-            >
+        <PlayerPreview avatar={info.avatar_url} username={info.login}>
+            <ul className="space-list-items">
+                {info.name && <li>{info.name}</li>}
+                {info.location && <li>{info.location}</li>}
+                {info.company && <li>{info.company}</li>}
+                <li>Followers: {info.followers}</li>
+                <li>Following: {info.following}</li>
+                <li>Public Repos: {info.public_repos}</li>
+                {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+            </ul>
             </PlayerPreview>
     )
 }
